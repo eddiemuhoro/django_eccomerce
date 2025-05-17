41b6217238fa224bf3a9d0f25aed36a594929109
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-17 12:04:10 +0300

    Add MpesaPaymentView for processing payment requests

--- api/views.py
+++ api/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django_daraja.mpesa.core import MpesaClient
-from .utils import get_access_token
+from .utils import get_access_token, lipa_na_mpesa_online
+from .serializers import MpesaPaymentSerializer
 
 # Create your views here.
 class MpesaTokenView(APIView):
@@ -13,3 +14,13 @@ class MpesaTokenView(APIView):
             return Response({"access_token": token}, status=200)
         except Exception as e:
             return Response({"error": str(e)}, status=500)
+        
+class MpesaPaymentView(APIView):
+    def post(self, request):
+        serializer = MpesaPaymentSerializer(data=request.data)
+        if serializer.is_valid():
+            phone_number = serializer.validated_data['phone_number']
+            amount = serializer.validated_data['amount']
+            response = lipa_na_mpesa_online(phone_number, amount)
+            return Response(response, status=200)
+        return Response(serializer.errors, status=400)
