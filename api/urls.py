299b7c39250a8a0e2903ee41619389b88a9f8320
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-17 12:17:46 +0300

    Add payment route to urlpatterns for MpesaPaymentView

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
-from .views import MpesaTokenView
+from .views import MpesaTokenView, MpesaPaymentView
 from django.urls import path
 
 urlpatterns = [
     path('token/', MpesaTokenView.as_view(), name='mpesa_token'),
+    path('pay/', MpesaPaymentView.as_view(), name='mpesa_payment'),
 ]
