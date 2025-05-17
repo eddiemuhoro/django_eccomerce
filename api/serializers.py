4223d2e0a724b19dc9b4f80c9e13ed9926936795
eddiemuhoro <eddiemuhoro@gmail.com>
2025-05-17 11:59:10 +0300

    Add MpesaPaymentSerializer for handling payment data

---
+++ api/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class MpesaPaymentSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(max_length=15)
+    amount = serializers.DecimalField(max_digits=10, decimal_places=2)
