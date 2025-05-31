a1f16b1cc713ec393c27fab65219ff9f154409bc
eddiemuhoro <eddiemuhoro@gmail.com>
2025-06-01 02:03:41 +0300

    Add additional products to seed_products management command

--- product/management/commands/seed_products.py
+++ product/management/commands/seed_products.py
@@ -24,6 +24,48 @@ class Command(BaseCommand):
                 "price": 300.00,
                 "quantity": 8,
             },
+            {
+                "name": "Product 4",
+                "desc": "Description for product 4",
+                "price": 150.00,
+                "quantity": 12,
+            },
+            {
+                "name": "Product 5",
+                "desc": "Description for product 5",
+                "price": 250.00,
+                "quantity": 7,
+            },
+            {
+                "name": "Product 6",
+                "desc": "Description for product 6",
+                "price": 175.00,
+                "quantity": 15,
+            },
+            {
+                "name": "Product 7",
+                "desc": "Description for product 7",
+                "price": 220.00,
+                "quantity": 3,
+            },
+            {
+                "name": "Product 8",
+                "desc": "Description for product 8",
+                "price": 90.00,
+                "quantity": 20,
+            },
+            {
+                "name": "Product 9",
+                "desc": "Description for product 9",
+                "price": 120.00,
+                "quantity": 6,
+            },
+            {
+                "name": "Product 10",
+                "desc": "Description for product 10",
+                "price": 180.00,
+                "quantity": 4,
+            },
         ]
 
         for prod in products:
